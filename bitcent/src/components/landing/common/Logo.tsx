4ff1742aafb438255d51645f0a1e4a5ba8acb537
Wenderson Júnio <wendersonjps@gmail.com>
2023-05-11 18:48:57 -0300

    :sparkles: feat: Added common components

---
+++ bitcent/src/components/landing/common/Logo.tsx
@@ -0,0 +1,8 @@
+export default function Logo() {
+    return (
+        <div className='text-3xl'>
+            <span className='font-black'>Bit</span>
+            <span className='text-indigo-600 font-thin'>Cent</span>
+        </div>
+    )
+}
