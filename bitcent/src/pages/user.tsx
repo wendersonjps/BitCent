a16660decb93ebf6d7b8191b5fdfc1f5d68341f1
Wenderson Júnio <wendersonjps@gmail.com>
2023-05-12 18:08:48 -0300

    :sparkles: feat: Added user page

---
+++ bitcent/src/pages/user.tsx
@@ -0,0 +1,14 @@
+import Page from '@/components/template/Page'
+import Header from '@/components/template/Header'
+import Content from '@/components/template/Content'
+
+export default function UserRegister() {
+    return (
+        <Page>
+            <Header />
+            <Content>
+                Usuário
+            </Content>
+        </Page>
+    )
+}
