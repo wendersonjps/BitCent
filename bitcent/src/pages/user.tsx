d7021c6cb433cdf1b124a8a7af20fba116598b90
Wenderson Júnio <wendersonjps@gmail.com>
2023-05-12 20:30:21 -0300

    :sparkles: Integrating params and imports

--- bitcent/src/pages/user.tsx
+++ bitcent/src/pages/user.tsx
@@ -1,13 +1,23 @@
 import Page from '@/components/template/Page'
 import Header from '@/components/template/Header'
 import Content from '@/components/template/Content'
+import TitlePage from '@/components/template/TitlePage'
+import { IconForms } from '@tabler/icons-react'
+import user from '@/data/constants/fakeUser'
+import Forms from '@/components/user/Forms'
 
 export default function UserRegister() {
+
     return (
         <Page>
             <Header />
             <Content>
-                Usuário
+                <TitlePage
+                    icon={<IconForms />}
+                    main='Dados Cadastrais'
+                    secondary={`Informações de ${user.email}`}
+                />
+                <Forms />
             </Content>
         </Page>
     )
