cebd5265a9f58a7758761144b41ba5b48c3c641b
Wenderson Júnio <wendersonjps@gmail.com>
2023-05-15 12:18:44 -0300

    :sparkles: feat: Integrating auth provider

--- bitcent/src/pages/_app.tsx
+++ bitcent/src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { MantineProvider } from '@mantine/core'
+import { AuthProvider } from '@/data/contexts/AuthContext'
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <MantineProvider theme={{ colorScheme: 'dark' }}>
-            <Component {...pageProps} />
+            <AuthProvider>
+                <Component {...pageProps} />
+            </AuthProvider>
         </MantineProvider>
     )
 }
