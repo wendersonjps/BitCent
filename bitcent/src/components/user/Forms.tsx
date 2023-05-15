57524117f8f1bb822ed40c29154c5941ebc95711
Wenderson Júnio <wendersonjps@gmail.com>
2023-05-15 12:18:31 -0300

    :sparkles: feat: Integrating auth context

--- bitcent/src/components/user/Forms.tsx
+++ bitcent/src/components/user/Forms.tsx
@@ -1,14 +1,26 @@
+import { useContext, useEffect } from 'react'
 import { TextInput } from '@mantine/core'
+import AuthContext from '@/data/contexts/AuthContext'
 import useForm from '@/data/hooks/useForm'
 import MiniForm from '../template/MiniForm'
-import user from '@/data/constants/fakeUser'
 import User from '@/logic/core/user/User'
 import Text from '@/logic/utils/Text'
 import Cpf from '@/logic/utils/Cpf'
 import Phone from '@/logic/utils/Phone'
 
 export default function Forms() {
-    const { data, changeAttribute } = useForm<User>(user)
+    const { user, userUpdate } = useContext(AuthContext)
+    const { data, changeAttribute, setData } = useForm<User>()
+
+    useEffect(() => {
+        if (!user) return
+        setData(user)
+    }, [user, setData])
+
+    async function save() {
+        if (!user) return
+        await userUpdate(data)
+    }
 
     return (
         <div className='flex flex-col mt-2 gap-5'>
@@ -17,7 +29,7 @@ export default function Forms() {
                 description='Como você gostaria de ser chamado?'
                 msgFooter='O nome deve possuir entre 3 e 80 caracteres!'
                 canSave={Text.between(data.name, 3, 80)}
-                save={() => {}}
+                save={save}
             >
                 <TextInput
                     value={data.name}
@@ -29,7 +41,7 @@ export default function Forms() {
                 description='Seu CPF é usado internamente pelo sistema.'
                 msgFooter='Pode relaxar, daqui ele não sai!'
                 canSave
-                save={() => {}}
+                save={save}
             >
                 <TextInput
                     value={Cpf.format(data.cpf ?? '')}
@@ -41,7 +53,7 @@ export default function Forms() {
                 description='Usado para notificações importantes sobre a sua conta.'
                 msgFooter='Se receber ligação a cobrar, não fomos nós!'
                 canSave
-                save={() => {}}
+                save={save}
             >
                 <TextInput
                     value={Phone.format(data.phone ?? '')}
--- bitcent/src/pages/index.tsx
+++ bitcent/src/pages/index.tsx
@@ -1,8 +1,12 @@
+import { useContext } from 'react'
+import AuthContext from '@/data/contexts/AuthContext'
 import Landing from '@/components/landing'
 import Finances from '@/components/finances'
+import Loading from '@/components/template/Loading'
 
 export default function Home() {
-    return (
-            <Finances />
-    )
+    const { user, loading } = useContext(AuthContext)
+
+    if (loading) return <Loading />
+    return user ? <Finances /> : <Landing />
 }
