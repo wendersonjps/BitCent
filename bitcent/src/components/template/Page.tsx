4ab67be1abe67a3d7a1e94cf7da53821a27ae588
Wenderson Júnio <wendersonjps@gmail.com>
2023-05-15 12:17:57 -0300

    :sparkles: feat: Integrating force auth

--- bitcent/src/components/template/Page.tsx
+++ bitcent/src/components/template/Page.tsx
@@ -1,3 +1,5 @@
+import ForceAuth from '../auth/ForceAuth'
+
 interface PageProps {
     external?: boolean
     children: any
@@ -5,15 +7,19 @@ interface PageProps {
 }
 
 export default function Page(props: PageProps) {
-    return (
-        <div
-            className={`
-                flex flex-col min-h-screen
-                bg-gradient-to-r from-zinc-900 via-black to-zinc-900
-                ${props.className ?? ''}
-            `}
-        >
-            {props.children}
-        </div>
-    )
+    function render() {
+        return (
+            <div
+                className={`
+                    flex flex-col min-h-screen
+                    bg-gradient-to-r from-zinc-900 via-black to-zinc-900
+                    ${props.className ?? ''}
+                `}
+            >
+                {props.children}
+            </div>
+        )
+    }
+
+    return props.external ? render() : <ForceAuth>{render()}</ForceAuth>
 }
