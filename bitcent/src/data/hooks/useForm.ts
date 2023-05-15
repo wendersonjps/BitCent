81f8f2156f11e6ecfea06abf32c8068e5290b0b7
Wenderson Júnio <wendersonjps@gmail.com>
2023-05-15 12:19:00 -0300

    :sparkles: feat: Integrating usecallback

--- bitcent/src/data/hooks/useForm.ts
+++ bitcent/src/data/hooks/useForm.ts
@@ -1,14 +1,14 @@
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 
-export default function useForm<T = any>(initialData: T) {
-    const [data, setData] = useState<T>(initialData)
+export default function useForm<T = any>(initialData?: T) {
+    const [data, setData] = useState<T>(initialData ?? {} as T)
 
-    function changeAttribute(attribute: string, fn?: Function) {
+    const changeAttribute = useCallback(function (attribute: string, fn?: Function) {
         return (valueOrEvent: any) => {
             const v = valueOrEvent?.target?.value ?? valueOrEvent
             setData({ ...data, [attribute]: fn?.(v) ?? v })
         }
-    }
+    }, [data])
 
     return {
         data,
